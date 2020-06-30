62f81d50111bc9e353f02d49451e3b40fcf6b88d
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2020-07-01 00:06:43 +0800

    simple bank

---
+++ playground/src/main/java/org/example/Bank.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bank {
+    private int funds = 100;
+    private final Lock lock = new ReentrantLock();
+
+    public void deposit(int money) {
+        lock.lock();
+        try {
+            funds += money;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void withdraw(int money) {
+        lock.lock();
+        try {
+            funds -= money;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
